91e9287cdbeea55cb05aa5ef664e4af65e6816bc
MitchStockley <63961154+MitchStockley@users.noreply.github.com>
2023-07-06 12:11:53 +0200

    Add files via upload

---
+++ Chapter5/chapter5.js
@@ -0,0 +1,19 @@
+document.getElementById("myName").innerHTML = "Mitchie";
+let item1;
+let item2;
+let item3;
+
+document.getElementById("changeList").onclick = newList;
+
+function newList() {
+    item1 = prompt("Enter a new first item");
+    item2 = prompt("Enter a new second item");
+    item3 = prompt("Enter a new third item");
+    updateList();
+}
+
+function updateList() {
+    document.getElementById("firstThing").innerHTML = item1;
+    document.getElementById("secondThing").innerHTML = item2;
+    document.getElementById("thirdThing").innerHTML = item3;
+}
