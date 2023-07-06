e2cd4bebe7402583f17b22994a6bce6b4fdb73d4
MitchStockley <63961154+MitchStockley@users.noreply.github.com>
2023-07-06 11:38:15 +0200

    Add files via upload

---
+++ Chapter3/chapter3.js
@@ -0,0 +1,7 @@
+let toName = "Mitchell";
+let fromName = "The grammy awards";
+let letterBody = "We are pleased to inform you that your song,Can\'t Stop Coding!,' has been voted the Best Song of All Time by the awarding committee.";
+document.write("<strong> Dear " + toName + ", </strong><br><br>");
+document.write(letterBody + "<br><br>");
+document.write("<strong> sincerely,<br>");
+document.write(fromName);
