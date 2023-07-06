80a2690fa85b205d6f3b105dc0b140bb1fb1cd49
MitchStockley <63961154+MitchStockley@users.noreply.github.com>
2023-07-06 11:44:19 +0200

    Add files via upload

---
+++ Chapter11/chapter11.js
@@ -0,0 +1,52 @@
+// let favoriteFoods = ["broccoli","eggplant",
+// "tacos","mushrooms"];
+
+// alert(favoriteFoods[1]);
+
+// let myArray = [5, "Hi there", true];
+// alert(myArray[1]);
+
+// let firstName = "Neil";
+// let middleName = "deGrasse"
+// let lastName = "Tyson";
+// let Scientist = [firstName, middleName, lastName];
+
+// alert(Scientist[1]);
+
+// let people = ["judy","Bobby", "Herbie", "tony", "Mary"];
+// people[0] = "James";
+// alert(people);
+
+
+let people = ["Kareem", "Mommy", "Sumaya","Fatma", "Mommy", "Ms. Young", "Mrs. O' Doherty"];
+let otherPeople = ["Emilie", "Mommy", "Dad", "Kathy"];
+
+// document.getElementById("peopleIKnow").innerHTML = people.valueOf();
+// document.getElementById("peopleIKnow").innerHTML = people.concat(otherPeople);
+
+
+//  people = people.indexOf("Fatma");
+//  alert(people);
+
+// people = people.join(" # ");
+// alert(people);
+
+//  people.push("Teddy");
+// alert(people);
+
+// people = people.reverse();
+// alert(people);
+
+// people.unshift("Teddy");
+// alert(people);
+
+// people = people.slice(0,1);
+// alert(people);
+
+// people = people.sort();
+// alert(people);
+
+ otherPeople = otherPeople.slice(0,3);
+alert(otherPeople);
+
+
